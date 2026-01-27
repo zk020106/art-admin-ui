a96723a424392d730de866c5e468f99ebb65133e
ppxb <317842449@qq.com>
2026-01-27 14:10:55 +0800

    refactor: remove prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: {
     html: true,
-    json: true
+    json: true,
   },
   rules: {
-    'regexp/no-unused-capturing-group': 'off'
-  }
+    'regexp/no-unused-capturing-group': 'off',
+  },
 })
