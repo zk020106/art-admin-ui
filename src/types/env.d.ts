2defb20a513185a982c5d369d8f3925cf5dd69b8
ppxb <317842449@qq.com>
2026-01-28 10:56:43 +0800

    perf: update vite env config

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,18 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_APP_VERSION: string
+  readonly VITE_APP_PORT: string
+  readonly VITE_BASE_URL: string
+  readonly VITE_API_URL: string
+  readonly VITE_API_PROXY_URL: string
+  readonly VITE_ENABLE_DEVTOOLS: string
+}
+
+interface importMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module 'nprogress'
 
 declare module 'crypto-js'
