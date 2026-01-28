e41f0c9bd3b5ed5a44027863c68907a963119fc8
ppxb <317842449@qq.com>
2026-01-28 10:57:34 +0800

    refactor: update main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { initStore } from './store' // Store
 import { setupErrorHandle } from './utils/sys/error-handle'
 import '@styles/core/tailwind.css' // tailwind
 import '@styles/index.scss' // 样式
-import '@utils/sys/console.ts' // 控制台输出内容
 
 document.addEventListener(
   'touchstart',
@@ -15,11 +14,16 @@ document.addEventListener(
   { passive: false },
 )
 
-const app = createApp(App)
-initStore(app)
-initRouter(app)
-setupGlobDirectives(app)
-setupErrorHandle(app)
+function bootstrap() {
+  const app = createApp(App)
 
-app.use(language)
-app.mount('#app')
+  initStore(app)
+  initRouter(app)
+  setupGlobDirectives(app)
+  setupErrorHandle(app)
+
+  app.use(language)
+  app.mount('#app')
+}
+
+bootstrap()
